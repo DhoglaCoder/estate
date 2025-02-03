87c98d4eded3f0bb37f42b2858e44d56298a3fc5
Harsh Agarwal <148757142+DhoglaCoder@users.noreply.github.com>
2025-02-03 22:15:30 +0530

    completed chat

--- frontend/src/components/Firebase/firebase.js
+++ frontend/src/components/Firebase/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCy8duX1sUVO2cQgn7ruPdYPlVK3cKTghE",
-  authDomain: "estate-react-app-9c9b2.firebaseapp.com",
-  projectId: "estate-react-app-9c9b2",
-  storageBucket: "estate-react-app-9c9b2.firebasestorage.app",
-  messagingSenderId: "170874099613",
-  appId: "1:170874099613:web:6c0afb55dd6082503e8174",
-  measurementId: "G-SEDWWVKF4L"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
