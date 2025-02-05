fc73b2e091b23e51db66c0497752db60890efc80
Harsh Agarwal <148757142+DhoglaCoder@users.noreply.github.com>
2025-02-05 21:38:35 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,33 @@
-import express from "express"
-import cors from "cors"
-import path from "path"
+import express from "express";
+import cors from "cors";
+import path from "path";
 import { connectDb } from "./config/db.js";
 import bodyParser from "body-parser";
-import listingRoutes from "./routes/listRoute.js"
+import listingRoutes from "./routes/listRoute.js";
 
 const app = express();
-const port = 3000
 
-//middleware
-app.use(express.json())
-app.use(cors(
-    {
-        origin:[],
-        methods:["POST","GET"],
-        credentials:true
-    }
-))
-app.use('/uploads',express.static(path.join(process.cwd(),'uploads')));
+// Middleware
+app.use(express.json());
+app.use(cors({
+    origin: [],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
+
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-//db connection
+// DB connection
 connectDb();
 
+// API routes
 app.use('/api/listings', listingRoutes);
 
-app.get("/",(req,res)=>{
+// Test route
+app.get("/", (req, res) => {
     res.send("API working");
-})
+});
 
-app.listen(port,()=>{
-    console.log("Server is running");
-})
+// Export the app for Vercel
+export default app;
